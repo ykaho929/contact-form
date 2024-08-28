dfc4574f28d1134d9905de53886cc57240d7784a
ykaho <h12.929.kaho@gmail.com>
2024-08-28 11:36:27 +0900

    8回目の変更

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,37 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Contact Form</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+@section('css')
   <link rel="stylesheet" href="{{ asset('css/confirm.css') }}" />
-</head>
+@endsection
 
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
-<body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Contact Form
-      </a>
-    </div>
-  </header>
-
-  <main>
+@section('content')
     <div class="confirm__content">
       <div class="confirm__heading">
         <h2>お問い合わせ内容確認</h2>
       </div>
+      <?php print_r($contact['name']) ?>
       <form class="form" actiom="/contacts" method="post">
         @csrf
         <div class="confirm-table">
@@ -67,7 +45,4 @@
         </div>
       </form>
     </div>
-  </main>
-</body>
-
-</html>
+@endsection
